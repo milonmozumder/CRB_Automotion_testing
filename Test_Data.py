ddf9506f36a4b0849338bf8e64808db294eab822
milonmozumder <milonmozumder71@gmail.com>
2024-12-07 16:12:17 +0600

    CRB_Automotion testing

---
+++ Test_Data.py
@@ -0,0 +1,37 @@
+test_data_set_one_hour = {
+    "start_time": {
+        "month": "12",
+        "day": "30",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "end_time": {
+        "month": "12",
+        "day": "30",
+        "year": "2024",
+        "hour": "01",
+        "minute": "00"
+    },
+    "expected_cost_for_small_room": "Total Cost: $100",
+    "expected_cost_for_medium_room": "Total Cost: $150",
+    "expected_cost_for_large_room": "Total Cost: $200",
+    "expected_cost_for_executive_room": "Total Cost: $300",
+}
+
+test_data_set_five_hour = {
+    "start_time": {
+        "month": "12",
+        "day": "30",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "end_time": {
+        "month": "12",
+        "day": "30",
+        "year": "2024",
+        "hour": "05",
+        "minute": "00"
+    }
+}
