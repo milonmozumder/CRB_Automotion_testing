09e79d0ade7c898d8a78ebca4a924c9e5aa3e90a
milonmozumder <milonmozumder71@gmail.com>
2024-12-09 13:18:43 +0600

    update all automation test

---
+++ Large_Room_Booking_Test/Test_Large_Room_Booking.py
@@ -0,0 +1,212 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from Test_Data import test_data_set_one_hour, test_data_set_five_hour, test_data_set_one_day, test_data_set_three_day, \
+    test_data_set_seven_day
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.get("https://muntasir101.github.io/Conference-Room-Booking/")
+    driver.implicitly_wait(20)
+
+    yield driver
+    driver.quit()
+
+
+def test_room_booking_large_room_one_hour(driver):
+    start_time_data = test_data_set_one_hour["start_time"]
+    end_time_data = test_data_set_one_hour["end_time"]
+    Select_room = driver.find_element(By.CSS_SELECTOR, "select#room")
+    room_type = Select(Select_room)
+    room_type.select_by_value("Large Room")
+
+    Start_time = driver.find_element(By.CSS_SELECTOR, "input#start-time")
+    Start_time.click()
+    Start_time.send_keys(start_time_data["month"])
+    Start_time.send_keys(start_time_data["day"])
+    Start_time.send_keys(start_time_data["year"])
+    Start_time.send_keys(start_time_data["hour"])
+    Start_time.send_keys(start_time_data["minute"])
+
+    End_time = driver.find_element(By.CSS_SELECTOR, "input#end-time")
+    End_time.click()
+    End_time.send_keys(end_time_data["month"])
+    End_time.send_keys(end_time_data["day"])
+    End_time.send_keys(end_time_data["year"])
+    End_time.send_keys(end_time_data["hour"])
+    End_time.send_keys(end_time_data["minute"])
+    End_time.send_keys(Keys.ARROW_DOWN)
+
+    Book_room_button = driver.find_element(By.CSS_SELECTOR, "#booking-form button")
+    Book_room_button.click()
+
+    #Expected_cost_for_one_hour = "Total Cost: $200"
+
+    Actual_cost_for_one_hour_element = driver.find_element(By.CSS_SELECTOR, "#cost")
+    Actual_cost_for_one_hour = Actual_cost_for_one_hour_element.text
+
+    if test_data_set_one_hour["expected_cost_for_large_room"] == Actual_cost_for_one_hour:
+        print("Test Case Passed.Cost for one hour is $200")
+    else:
+        print("Test Case Failed.")
+        print("Expected Cost: ", test_data_set_one_hour["expected_cost_for_large_room"], "But Found: ", Actual_cost_for_one_hour)
+
+
+def test_room_booking_large_room_five_hour(driver):
+    start_time_data = test_data_set_five_hour["start_time"]
+    end_time_data = test_data_set_five_hour["end_time"]
+    Select_room = driver.find_element(By.CSS_SELECTOR, "select#room")
+    room_type = Select(Select_room)
+    room_type.select_by_value("Large Room")
+
+    Start_time = driver.find_element(By.CSS_SELECTOR, "input#start-time")
+    Start_time.click()
+    Start_time.send_keys(start_time_data["month"])
+    Start_time.send_keys(start_time_data["day"])
+    Start_time.send_keys(start_time_data["year"])
+    Start_time.send_keys(start_time_data["hour"])
+    Start_time.send_keys(start_time_data["minute"])
+
+    End_time = driver.find_element(By.CSS_SELECTOR, "input#end-time")
+    End_time.click()
+    End_time.send_keys(end_time_data["month"])
+    End_time.send_keys(end_time_data["day"])
+    End_time.send_keys(end_time_data["year"])
+    End_time.send_keys(end_time_data["hour"])
+    End_time.send_keys(end_time_data["minute"])
+    End_time.send_keys(Keys.ARROW_DOWN)
+
+    Book_room_button = driver.find_element(By.CSS_SELECTOR, "#booking-form button")
+    Book_room_button.click()
+
+    #Expected_cost_for_five_hour = "Total Cost: $750"
+
+    Actual_cost_for_five_hour_element = driver.find_element(By.CSS_SELECTOR, "#cost")
+    Actual_cost_for_five_hour = Actual_cost_for_five_hour_element.text
+
+    if test_data_set_five_hour["expected_cost_for_large_room"] == Actual_cost_for_five_hour:
+        print("Test Case Passed.Cost for five hour is $1000")
+    else:
+        print("Test Case Failed.")
+        print("Expected Cost: ", test_data_set_five_hour["expected_cost_for_large_room"], "But Found: ", Actual_cost_for_five_hour)
+
+def test_room_booking_large_room_one_day(driver):
+    start_time_data = test_data_set_one_day["start_time"]
+    end_time_data = test_data_set_one_day["end_time"]
+    Select_room = driver.find_element(By.CSS_SELECTOR, "select#room")
+    room_type = Select(Select_room)
+    room_type.select_by_value("Large Room")
+
+    Start_time = driver.find_element(By.CSS_SELECTOR, "input#start-time")
+    Start_time.click()
+    Start_time.send_keys(start_time_data["month"])
+    Start_time.send_keys(start_time_data["day"])
+    Start_time.send_keys(start_time_data["year"])
+    Start_time.send_keys(start_time_data["hour"])
+    Start_time.send_keys(start_time_data["minute"])
+
+    End_time = driver.find_element(By.CSS_SELECTOR, "input#end-time")
+    End_time.click()
+    End_time.send_keys(end_time_data["month"])
+    End_time.send_keys(end_time_data["day"])
+    End_time.send_keys(end_time_data["year"])
+    End_time.send_keys(end_time_data["hour"])
+    End_time.send_keys(end_time_data["minute"])
+    #End_time.send_keys(Keys.ARROW_DOWN)
+
+    Book_room_button = driver.find_element(By.CSS_SELECTOR, "#booking-form button")
+    Book_room_button.click()
+
+    #Expected_cost_for_one_day = "Total Cost: $1200"
+
+    Actual_cost_for_one_day_element = driver.find_element(By.CSS_SELECTOR, "#cost")
+    Actual_cost_for_one_day = Actual_cost_for_one_day_element.text
+
+    if test_data_set_one_day["expected_cost_for_large_room"] == Actual_cost_for_one_day:
+        print("Test Case Passed.Cost for one day is $1600")
+    else:
+        print("Test Case Failed.")
+        print("Expected Cost: ", test_data_set_one_day["expected_cost_for_large_room"], "But Found: ", Actual_cost_for_one_day)
+
+def test_room_booking_large_room_three_day(driver):
+    start_time_data = test_data_set_three_day["start_time"]
+    end_time_data = test_data_set_three_day["end_time"]
+    Select_room = driver.find_element(By.CSS_SELECTOR, "select#room")
+    room_type = Select(Select_room)
+    room_type.select_by_value("Large Room")
+
+    Start_time = driver.find_element(By.CSS_SELECTOR, "input#start-time")
+    Start_time.click()
+    Start_time.send_keys(start_time_data["month"])
+    Start_time.send_keys(start_time_data["day"])
+    Start_time.send_keys(start_time_data["year"])
+    Start_time.send_keys(start_time_data["hour"])
+    Start_time.send_keys(start_time_data["minute"])
+
+    End_time = driver.find_element(By.CSS_SELECTOR, "input#end-time")
+    End_time.click()
+    End_time.send_keys(end_time_data["month"])
+    End_time.send_keys(end_time_data["day"])
+    End_time.send_keys(end_time_data["year"])
+    End_time.send_keys(end_time_data["hour"])
+    End_time.send_keys(end_time_data["minute"])
+    #End_time.send_keys(Keys.ARROW_DOWN)
+
+    Book_room_button = driver.find_element(By.CSS_SELECTOR, "#booking-form button")
+    Book_room_button.click()
+
+    #Expected_cost_for_three_day = "Total Cost: $3600"
+
+    Actual_cost_for_three_day_element = driver.find_element(By.CSS_SELECTOR, "#cost")
+    Actual_cost_for_three_day = Actual_cost_for_three_day_element.text
+
+    if test_data_set_three_day["expected_cost_for_large_room"] == Actual_cost_for_three_day:
+        print("Test Case Passed.Cost for three day is $4800")
+    else:
+        print("Test Case Failed.")
+        print("Expected Cost: ", test_data_set_three_day["expected_cost_for_large_room"], "But Found: ", Actual_cost_for_three_day)
+
+def test_room_booking_large_room_seven_day(driver):
+    start_time_data = test_data_set_seven_day["start_time"]
+    end_time_data = test_data_set_seven_day["end_time"]
+    Select_room = driver.find_element(By.CSS_SELECTOR, "select#room")
+    room_type = Select(Select_room)
+    room_type.select_by_value("Large Room")
+
+    Start_time = driver.find_element(By.CSS_SELECTOR, "input#start-time")
+    Start_time.click()
+    Start_time.send_keys(start_time_data["month"])
+    Start_time.send_keys(start_time_data["day"])
+    Start_time.send_keys(start_time_data["year"])
+    Start_time.send_keys(start_time_data["hour"])
+    Start_time.send_keys(start_time_data["minute"])
+
+    End_time = driver.find_element(By.CSS_SELECTOR, "input#end-time")
+    End_time.click()
+    End_time.send_keys(end_time_data["month"])
+    End_time.send_keys(end_time_data["day"])
+    End_time.send_keys(end_time_data["year"])
+    End_time.send_keys(end_time_data["hour"])
+    End_time.send_keys(end_time_data["minute"])
+    #End_time.send_keys(Keys.ARROW_DOWN)
+
+    Book_room_button = driver.find_element(By.CSS_SELECTOR, "#booking-form button")
+    Book_room_button.click()
+
+    #Expected_cost_for_seven_day = "Total Cost: $11200"
+
+    Actual_cost_for_seven_day_element = driver.find_element(By.CSS_SELECTOR, "#cost")
+    Actual_cost_for_seven_day = Actual_cost_for_seven_day_element.text
+
+    if test_data_set_seven_day["expected_cost_for_large_room"] == Actual_cost_for_seven_day:
+        print("Test Case Passed.Cost for seven day is $11200")
+    else:
+        print("Test Case Failed.")
+        print("Expected Cost: ", test_data_set_seven_day["expected_cost_for_large_room"], "But Found: ", Actual_cost_for_seven_day)
+
+
+
--- Test_Data.py
+++ Test_Data.py
@@ -1,14 +1,14 @@
 test_data_set_one_hour = {
     "start_time": {
         "month": "12",
-        "day": "30",
+        "day": "07",
         "year": "2024",
         "hour": "12",
         "minute": "00"
     },
     "end_time": {
         "month": "12",
-        "day": "30",
+        "day": "07",
         "year": "2024",
         "hour": "01",
         "minute": "00"
@@ -22,16 +22,82 @@ test_data_set_one_hour = {
 test_data_set_five_hour = {
     "start_time": {
         "month": "12",
-        "day": "30",
+        "day": "07",
         "year": "2024",
         "hour": "12",
         "minute": "00"
     },
     "end_time": {
         "month": "12",
-        "day": "30",
+        "day": "07",
         "year": "2024",
         "hour": "05",
         "minute": "00"
-    }
+    },
+    "expected_cost_for_small_room": "Total Cost: $500",
+    "expected_cost_for_medium_room": "Total Cost: $750",
+    "expected_cost_for_large_room": "Total Cost: $1000",
+    "expected_cost_for_executive_room": "Total Cost: $1500",
+}
+test_data_set_one_day = {
+    "start_time": {
+        "month": "12",
+        "day": "07",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "end_time": {
+        "month": "12",
+        "day": "08",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "expected_cost_for_small_room": "Total Cost: $800",
+    "expected_cost_for_medium_room": "Total Cost: $1200",
+    "expected_cost_for_large_room": "Total Cost: $1600",
+    "expected_cost_for_executive_room": "Total Cost: $2400",
+}
+
+test_data_set_three_day = {
+    "start_time": {
+        "month": "12",
+        "day": "07",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "end_time": {
+        "month": "12",
+        "day": "10",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "expected_cost_for_small_room": "Total Cost: $2600",
+    "expected_cost_for_medium_room": "Total Cost: $3600",
+    "expected_cost_for_large_room": "Total Cost: $4800",
+    "expected_cost_for_executive_room": "Total Cost: $7200",
+}
+
+test_data_set_seven_day = {
+    "start_time": {
+        "month": "12",
+        "day": "07",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "end_time": {
+        "month": "12",
+        "day": "14",
+        "year": "2024",
+        "hour": "12",
+        "minute": "00"
+    },
+    "expected_cost_for_small_room": "Total Cost: $5600",
+    "expected_cost_for_medium_room": "Total Cost: $8400",
+    "expected_cost_for_large_room": "Total Cost: $11200",
+    "expected_cost_for_executive_room": "Total Cost: $16800",
 }
